0a6de7c96490a73003ba77b4a16f961e9d352d18
cxrlos <A01025948@itesm.mx>
2020-11-03 20:07:21 -0600

    Added progress bar, plot to view losses but can't make the JSON_to_Spacy data work adequately with the spaCy training object

--- 2-Training/Training.py
+++ 2-Training/Training.py
@@ -12,15 +12,35 @@ import spacy
 import random
 import os
 import JSON_to_spaCy
+import matplotlib.pyplot as plt
+import sys, time
 from spacy.util import minibatch, compounding
 
 nlp = spacy.load('en_core_web_sm')
-ner = nlp.get_pipe("ner") # Getting pipeline component
+ner = nlp.get_pipe("ner") 
+
+max_iterations = 20
+
+# To better visualize the learning progress, copied from 
+# https://www.pythonexample.org/gui/how-to-draw-a-progress-bar-in-python/
+class ProgressBar:
+    def __init__(self, count = 0, total = 0, width = 50):
+        self.count = count
+        self.total = total
+        self.width = width
+    def move(self):
+        self.count += 1
+    def log(self, s):
+        sys.stdout.write(' ' * (self.width + 9) + '\r')
+        sys.stdout.flush()
+        progress = int(self.width * self.count / self.total)
+        sys.stdout.write('{0:3}/{1:3}: '.format(self.count, self.total))
+        sys.stdout.write('#' * progress + '-' * (self.width - progress) + '\r')
 
 # Read the file names in the labelling directory to pass them through the 
 # conversion algorithm
 files = []
-for (path, dirnames, filenames) in os.walk('../../Labeling/Data/experiment_results/labelling'):
+for (path, dirnames, filenames) in os.walk('../../Labelling/Data/experiment_results/labelling'):
     files.extend(os.path.join(path, name) for name in filenames)
 random.shuffle(files)
 
@@ -31,33 +51,17 @@ for x in files:
     current_file, json_text = JSON_to_spaCy.ConvertJSON(x)
     for y in current_file:
         training_set.extend((json_text, {"entities": [(y.start, y.end, y.d_type)]}))
+    print(json_text)
+    print(training_set)
+    print('\n')
+
 
 # TODO: Make previous assignation work as the following structure
-training_set = [
-    ("Walmart is a leading e-commerce company", {"entities": [(0, 7, "ORG")]}),
-    ("I reached Chennai yesterday.", {"entities": [(19, 28, "GPE")]}),
-    ("I recently ordered a book from Amazon", {"entities": [(24,32, "ORG")]}),
-    ("I was driving a BMW", {"entities": [(16,19, "PRODUCT")]}),
-    ("I ordered this from ShopClues", {"entities": [(20,29, "ORG")]}),
-    ("Fridge can be ordered in Amazon ", {"entities": [(0,6, "PRODUCT")]}),
-    ("I bought a new Washer", {"entities": [(16,22, "PRODUCT")]}),
-    ("I bought a old table", {"entities": [(16,21, "PRODUCT")]}),
-    ("I bought a fancy dress", {"entities": [(18,23, "PRODUCT")]}),
-    ("I rented a camera", {"entities": [(12,18, "PRODUCT")]}),
-    ("I rented a tent for our trip", {"entities": [(12,16, "PRODUCT")]}),
-    ("I rented a screwdriver from our neighbour", {"entities": [(12,22, "PRODUCT")]}),
-    ("I repaired my computer", {"entities": [(15,23, "PRODUCT")]}),
-    ("I got my clock fixed", {"entities": [(16,21, "PRODUCT")]}),
-    ("I got my truck fixed", {"entities": [(16,21, "PRODUCT")]}),
-    ("Flipkart started it's journey from zero", {"entities": [(0,8, "ORG")]}),
-    ("I recently ordered from Max", {"entities": [(24,27, "ORG")]}),
-    ("Flipkart is recognized as leader in market",{"entities": [(0,8, "ORG")]}),
-    ("I recently ordered from Swiggy", {"entities": [(24,29, "ORG")]})
-]
-
-print(training_set)
-for i in training_set:
-    print(i)
+# training_set = [
+#     ("Walmart is a leading e-commerce company", {"entities": [(0, 7, "ORG")]}),
+#     ("I reached Chennai yesterday.", {"entities": [(19, 28, "GPE")]}),
+#     ("I recently ordered a book from Amazon", {"entities": [(24,32, "ORG")]}),
+# ]
 
 for _, annotations in training_set:
   for ent in annotations.get("entities"):
@@ -66,16 +70,31 @@ for _, annotations in training_set:
 pipe_exceptions = ["ner", "trf_wordpiecer", "trf_tok2vec"]
 unaffected_pipes = [pipe for pipe in nlp.pipe_names if pipe not in pipe_exceptions]
 
+bar = ProgressBar(total = max_iterations)
+graph_data = []
+
+print('Training in process, please wait for a moment')
 with nlp.disable_pipes(*unaffected_pipes):
-  for iteration in range(30):
+  for iteration in range(max_iterations):
     random.shuffle(training_set)
     losses = {}
     batches = minibatch(training_set, size=compounding(4.0, 32.0, 1.001))
     for batch in batches:
         texts, annotations = zip(*batch)
         nlp.update(texts, annotations, drop=0.5, losses=losses)
-        print("Losses", losses)
+        # print("Losses", losses)
+        graph_data.append(losses['ner'])
+    bar.move()
+    bar.log('' + str(iteration+1))
+
 out_path = os.getcwd() + '/model'
 nlp.to_disk(out_path)
 print("Saved model to", out_path)
+
+# Plot loss graph to detemine easily thee correcctness of the learning params
+plt.plot(graph_data,':')
+plt.xlabel('Number of iterations')
+plt.ylabel('Loss')
+plt.title('Loss vs Iteratitons')
+plt.show()
 
